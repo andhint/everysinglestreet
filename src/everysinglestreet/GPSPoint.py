a16abaa876fa095ba4307679506b2c871cbda9ce
Andrew Hintermeier <andrew@Andrews-MacBook-Air.local>
2020-05-04 02:19:29 -0400

    removed GPSPoint file

--- src/everysinglestreet/GPSPoint.py
+++
@@ -1,9 +0,0 @@
-class GPSPoint:
-    def __init__(self, lat:float, lon:float, elevation, timestamp):
-        self.lat = lat
-        self.lon = lon
-        self.elevation = elevation
-        self.timestamp = timestamp
-
-    def print_point(self):
-       print('{} {} at time {} at elevation {}'.format(self.lat, self.lon, self.timestamp, self.elevation))
